2584ea880380593922bcddaab653d20ccfe619e9
jayanthi gunja <jayanthi.tech94@gmail.com>
2024-12-30 15:25:00 +0530

    conversation added

---
+++ 05-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Please enter your UserName::"
+
+read -s USERNAME
+
+echo "Username entered":$USERNAME
+
+echo "Please enter password::"
+
+read  PASSWORD
+
+echo "password entered"
+
+
