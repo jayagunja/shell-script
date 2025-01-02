1785dcac1c306a5cf06f33b8decdfc18ce3acbd5
jayanthi gunja <jayanthi.tech94@gmail.com>
2025-01-02 14:59:16 +0530

    datatypes

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -1,1 +1,14 @@
-# /bin/bash
+# !/bin/bash
+
+All variables passed: $@
+number of variables : $#
+script name : $0
+present working directory : $PWD 
+home directroy of current  user :$HOME 
+which user is running this script : $USER 
+process id of current script : $$
+process id of last command in background : $!
+
+
+
+
