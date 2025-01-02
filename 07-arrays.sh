f2dd12206d69aed6c22a90a56af6e38fb98a66ec
jayanthi gunja <jayanthi.tech94@gmail.com>
2025-01-02 13:33:36 +0530

    datatypes

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -1,9 +1,9 @@
 Movies=("RRR" "Bahubali" "Saho")
 
-echo "First movie :${MOvies[0]}"
+echo "First movie :${Movies[0]}"
 echo "Second movie :${Movies[1]}"
 echo "Third movie :${Movies[2]}"
 
-echo "All movie are :${movies[@]}"
+echo "All movie are :${Movies[@]}"
 
 
