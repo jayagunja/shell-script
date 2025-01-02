5f5e90cc35a7246e1b0cc8b3c9959a54e01bfcc6
jayanthi gunja <jayanthi.tech94@gmail.com>
2025-01-02 21:30:55 +0530

    datatypes

---
+++ 12-colors.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+R ="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2... $R Failure"
+        exit 1
+    else
+        echo -e "$2.. $G Success"
+    fi
+
+}
+
+USERID=$(id -u)
+ if [ $USERID -ne 0 ]
+ then
+    echo "Error:: you must have sudo access to execute this script"
+    exit 1 # other than 0
+fi   
+
+dnf list installed mysql
+if [ $? -ne 0 ] 
+then # not insatlled
+    dnf install mysql -y
+    VALIDATE $? "Installing.."
+ else
+    echo -e "mySql is already $G installed"
+ fi    
+   
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+     VALIDATE $? "Installing.."
+ 
+else
+    echo -e "git is already $G installed"
+fi
