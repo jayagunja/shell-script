cca567c5b75107fe620d08e21774323ee637b74f
jayanthi gunja <jayanthi.tech94@gmail.com>
2025-01-02 15:34:53 +0530

    datatypes

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -3,8 +3,8 @@
 NUMBER=$1
 
 if[$NUMBER -gt 100]
-    then
-    echo "Given number is greater than 100"
-    else
-    echo "Given number is less than 100"
+then
+echo "Given number is greater than 100"
+else
+echo "Given number is less than 100"
 fi
