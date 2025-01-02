ea1b5d803b6b84baf9785a55dc058d24ab101cce
jayanthi gunja <jayanthi.tech94@gmail.com>
2025-01-02 21:56:48 +0530

    datatypes

--- 13-logs.sh
+++ 13-logs.sh
@@ -4,6 +4,11 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
@@ -18,23 +23,23 @@ VALIDATE(){
 USERID=$(id -u)
  if [ $USERID -ne 0 ]
  then
-    echo "Error:: you must have sudo access to execute this script"
+    echo "Error:: you must have sudo access to execute this script" &>>$LOG_FILE_NAME
     exit 1 # other than 0
 fi   
 
-dnf list installed mysql
+dnf list installed mysql  &>>$LOG_FILE_NAME
 if [ $? -ne 0 ] 
 then # not insatlled
-    dnf install mysql -y
+    dnf install mysql -y  &>>$LOG_FILE_NAME
     VALIDATE $? "Installing.."
  else
     echo -e "mySql is already $G installed $N"
  fi    
    
-dnf list installed mysql
+dnf list installed mysql  &>>$LOG_FILE_NAME
 if [ $? -ne 0 ]
 then
-    dnf install git -y
+    dnf install git -y  &>>$LOG_FILE_NAME
      VALIDATE $? "Installing.."
  
 else
