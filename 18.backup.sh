5ec50c6d91343eb3c1b425c0c00ee3374ad4fada
jayanthi gunja <jayanthi.tech94@gmail.com>
2025-01-06 22:41:00 +0530

    backup

--- 18.backup.sh
+++ 18.backup.sh
@@ -9,7 +9,7 @@ DEST_DIR=$2
 DAYS=${3:-14} #If user not providing number of days, We are taking 14 as default
 
 # SOURCE_DIR="/home/ec2-user/app-logs"
-LOGS_FOLDER="/var/log/shellscript-logs"
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -28,6 +28,8 @@ VALIDATE(){
     echo -e "$R USAGE:: $Y sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>" $N
     exit 1
  }
+
+ mkdir -p $LOGS_FOLDER
  
  if [ $# -lt 2 ]
  then
