60cd91ef550d1a32ea20acc40cd404c34dd193cc
jayanthi gunja <jayanthi.tech94@gmail.com>
2025-01-07 21:56:16 +0530

    disk_usage

--- 19-disk-usage.sh
+++ 19-disk-usage.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-    DISK_USAGE=$(df -hT | grep xfs)
-    
-    while read -r line
-    do 
-        echo $line
-    done <<< $DISK_USAGE
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5 #real projects will monitor for 70
+MSG=""
+
+while read -r line
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    #echo "Partition: $PARTITION , Usage: $USAGE"
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE \n "
+    fi
+done <<< $DISK_USAGE
+
+echo -e "Message: $MSG"
+
+echo "$MSG" | mutt -s "High Disk Usage" jayanthig30119@gmail.com
