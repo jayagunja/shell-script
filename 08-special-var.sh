af88425f69f22efff0a06d874004a38814917a4d
jayanthi gunja <jayanthi.tech94@gmail.com>
2026-09-18 16:10:12 +0530

    arrays

--- 08-special-var.sh
+++ 08-special-var.sh
@@ -8,6 +8,7 @@ echo "home directroy of current  user :$HOME"
 echo "which user is running this script : $USER" 
 echo "process id of current script : $$"
 sleep 60 &
+#& denotes to moving foreground to backend running process
 echo "process id of last command in background : $!"
 
 
